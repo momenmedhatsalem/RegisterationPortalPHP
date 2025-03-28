f51fbfb4f0f2706f26385cac51ea6b730e9d9788
Nooruddin Ali <nooruddinma.ali@gmail.com>
2025-03-28 10:30:01 +0200

    Add creative header and logo to registration portal, tried to match footer style. logo is a bit ugly but it's just for starters

---
+++ templates/header.php
@@ -0,0 +1,33 @@
+<header class="registration-header">
+    <div class="container">
+        <div class="logo-container">
+            <img src="public/assets/img/university-logo.svg" alt="University Logo" class="logo">
+        </div>
+        <div class="row">
+            <div class="col">
+                <h4>Navigation</h4>
+                <ul>
+                    <li><a href="#">Home</a></li>
+                    <li><a href="#" class="active">Registration</a></li>
+                    <li><a href="#">Dashboard</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h4>Quick Links</h4>
+                <ul>
+                    <li><a href="#">Help Center</a></li>
+                    <li><a href="#">Support</a></li>
+                    <li><a href="#">Contact</a></li>
+                </ul>
+            </div>
+            <div class="col">
+                <h4>University Portal</h4>
+                <div class="social-links">
+                    <a href="#"><i class="fas fa-user"></i></a>
+                    <a href="#"><i class="fas fa-bell"></i></a>
+                    <a href="#"><i class="fas fa-cog"></i></a>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>

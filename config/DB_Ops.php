60d329812255b6b174d394e53c623997ef0ac24e
Ahmed ElAlfy <ahalfy2005@gmail.com>
2025-03-29 15:35:46 +0200

    First working version
    Added AJAX and ensured server-side validation and insertion into db.
    Still needs integration with wp validation API and image upload, and handling the http response

--- config/DB_Ops.php
+++ config/DB_Ops.php
@@ -1,6 +1,4 @@
 <?php
-    //include __DIR__ . '../app/services/API_Ops.php';
-
     $fullName = $userName = $email = $number = $wpNumber = $address = $password = $confirmPassword = "";
     
     $errMsgs["full_name"] = "";
@@ -45,39 +43,56 @@
             $shouldStoreIn_db = $shouldStoreIn_db && validate_whatsappPhoneNumber($wpNumber);
             $shouldStoreIn_db = $shouldStoreIn_db && validate_password($password);
             $shouldStoreIn_db = $shouldStoreIn_db && validate_confirmPassword($confirmPassword);
-        }
-
-        if ($shouldStoreIn_db)
-        {
-            $servername = "localhost";
-            $username = "root";
-            $password ="";
-            $db_name = "registrationportal";
-            $tableName = "users";
         
-            $conn = mysqli_connect($servername, $username, $password)
-            or die ("couldn't connect to this host, and the error is: " . mysqli_connect_error());
-
-            // Check if the database exists
-            $sql = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = " . $db_name;
-            $result = mysqli_query($conn, $sql);
-            if (mysqli_num_rows($result) <= 0) {
-                //TODO: code to create database
-            }
-
-            mysqli_select_db($conn, $db_name)
-            or die ("couldn't open this database, and the error is: " . mysqli_error($conn));
+            if ($shouldStoreIn_db)
+            {
+                $servername = "localhost";
+                $db_username = "root";
+                $db_password ="";
+                $db_name = "registrationportal";
+                $tableName = "users";
             
-            //user_image, before email
-            $sql = "INSERT INTO " . $tableName . "(full_name, user_name, phone, whatsapp_number, address, password, email)
-            VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+                $conn = mysqli_connect($servername, $db_username, $db_password)
+                or die ("couldn't connect to this host, and the error is: " . mysqli_connect_error());
+    
+                // Check if the database exists
+                $sql = "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$db_name'";
+                $result = mysqli_query($conn, $sql);
+                if (mysqli_num_rows($result) <= 0) {
+                    //TODO: code to create database
+                }
+    
+                mysqli_select_db($conn, $db_name)
+                or die ("couldn't open this database, and the error is: " . mysqli_error($conn));
+                
+                //user_image, before email
+                /*$sql = "INSERT INTO " . $tableName . "(full_name, user_name, phone, whatsapp_number, address, password, email)
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+    
+                $stmt = mysqli_prepare($conn, $sql);
+                mysqli_stmt_bind_param($stmt, "ssssssss", $fullname, $userName, $number, $wpNumber, $address, $password, $email);
+                mysqli_stmt_execute($stmt);
+    
+                mysqli_stmt_close($stmt);*/
 
-            $stmt = mysqli_prepare($conn, $sql);
-            mysqli_stmt_bind_param($stmt, "ssssssss", $fullname, $userName, $number, $wpNumber, $address, $password, $email);
-            mysqli_stmt_execute($stmt);
+                //Important note: an arbitraty image path is stored. Not the real input. To be changed later
+                $imagePath = "hi.png";
+                $query = "INSERT INTO $tableName (full_name, user_name, phone, whatsapp_number, address, password, user_image, email) VALUES ('$fullName', '$userName', '$number', '$wpNumber', '$address', '$password', '$imagePath', '$email')";
+                mysqli_query($conn, $query);
+                mysqli_close($conn);
 
-            mysqli_stmt_close($stmt);
-            mysqli_close($conn);
+                echo "You have successfully registered!";
+            }
+            else
+            {
+                header('Content-Type: application/json');
+                echo json_encode($errMsgs);
+            }
+        }
+        else
+        {
+            header('Content-Type: application/json');
+            echo json_encode($errMsgs);
         }
     }
 
@@ -85,7 +100,7 @@
         if (checkRequired($inputName))
         {
             $inputValue = trim($inputValue);
-            if (!preg_match("/.*[password|user_name]+.*/", $inputName))
+            if (!preg_match("/(password|user_name)/", $inputName))
             {
                 $inputValue = stripslashes($inputValue);
             }
@@ -97,6 +112,7 @@
     }
 
     function checkRequired ($inputName) {
+        global $errMsgs;
         if (empty($_POST[$inputName]))
         {
             $errMsgs[$inputName] = 'This field is required. Please fill it';
@@ -107,6 +123,7 @@
     }
 
     function validate_fullName ($fullName) {
+        global $errMsgs;
         if (preg_match("/^(?=.*[a-zA-Z])[a-zA-Z -']*$/", $fullName))
         {
             return true;
@@ -117,9 +134,11 @@
     }
 
     function validate_username ($userName) {
+        global $errMsgs;
         if (preg_match("/^[^\s]+$/", $userName))
         {
             //TODO: check it's uniqueness from the DB
+            return true;
         } else {
             $errMsgs["user_name"] = "usernames cannot contain any whitespaces";
             return false;
@@ -127,6 +146,7 @@
     }
 
     function validate_email ($email) {
+        global $errMsgs;
         if (filter_var($email, FILTER_VALIDATE_EMAIL))
         {
             return true;
@@ -137,7 +157,8 @@
     }
 
     function validate_phoneNumber ($phoneNumber) {
-        if (preg_match("/\+[0-9]{7,15}/", $phoneNumber)) {
+        global $errMsgs;
+        if (preg_match("/^\+?[0-9]{7,15}$/", $phoneNumber)) {
             return true;
         } else {
             $errMsgs["phone"] = "please enter your phone in the international format (starting with a +) and without any separating special characters between digits";
@@ -146,7 +167,8 @@
     }
 
     function validate_whatsappPhoneNumber ($WP_phoneNumber) {
-        if (validate_phoneNumber($WP_phoneNumber) && validateWhatsAppNumber($WP_phoneNumber))
+        global $errMsgs;
+        if (validate_phoneNumber($WP_phoneNumber))
         {
             return true;
         } else {
@@ -156,9 +178,11 @@
 
     function validate_password ($password) {
         //TODO
+        return true;
     }
 
     function validate_confirmPassword ($confirmPassword) {
+        return true;
         //TODO
     }
 ?>
--- index.php
+++ index.php
@@ -7,41 +7,41 @@
     <link rel="stylesheet" href="public/assets/css/style.css">
 </head>
 <body>
-<?php include 'templates/header.php'; include 'config/DB_Ops.php'?> 
+<?php include 'templates/header.php'?> 
     <div class="container">
-        <form id="registrationForm", method="post" enctype="multipart/form-data" action="Config/DB_Ops.php">
+        <form id="registrationForm" method="post" enctype="multipart/form-data" onsubmit="return submitForm(event)">
             <h2>Registration</h2>
 
             <div class="input-group">
                 <div>
                     <label for="full_name">Full Name</label>
-                    <input type="text" id="full_name" name="full_name" placeholder="Enter your full name" value="<?php echo $fullName ?>" required>
+                    <input type="text" id="full_name" name="full_name" placeholder="Enter your full name" required>
                 </div>
                 <div>
                     <label for="user_name">Username</label>
-                    <input type="text" id="user_name" name="user_name" placeholder="Enter your username" value="<?php echo $userName ?>" required>
+                    <input type="text" id="user_name" name="user_name" placeholder="Enter your username" required>
                 </div>
             </div>
 
             <div class="input-group">
                 <div>
                     <label for="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder="Enter your email" value="<?php echo $email ?>" required>
+                    <input type="email" id="email" name="email" placeholder="Enter your email" required>
                 </div>
                 <div>
                     <label for="phone">Phone Number</label>
-                    <input type="tel" id="phone" name="phone" placeholder="Enter your phone number" value="<?php echo $number ?>" required>
+                    <input type="tel" id="phone" name="phone" placeholder="Enter your phone number" required>
                 </div>
             </div>
 
             <div class="input-group">
                 <div>
                     <label for="whatsapp">WhatsApp Number</label>
-                    <input type="tel" id="whatsapp" name="whatsapp" placeholder="Enter your WhatsApp number" value="<?php echo $wpNumber ?>" required>
+                    <input type="tel" id="whatsapp" name="whatsapp" placeholder="Enter your WhatsApp number" required>
                 </div>
                 <div>
                     <label for="address">Address</label>
-                    <input type="text" id="address" name="address" placeholder="Enter your address" value="<?php echo $address ?>" required>
+                    <input type="text" id="address" name="address" placeholder="Enter your address" required>
                 </div>
             </div>
 
@@ -66,10 +66,39 @@
             <button type="submit" class="btn">Register</button>
         </form>
     </div>
+<?php include 'templates/footer.php'?>
 
-    <script src="script.js"></script>
-    
-<?php include 'templates/footer.php'?> 
+<script src="script.js"></script>
+<script>
+    function submitForm(event) {
+        event.preventDefault();
+
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open('POST', 'config/DB_Ops.php', true);
+        //xmlHttp.setRequestHeader('Content-Type', 'multipart/form-data');
+
+        var formData = new FormData(document.getElementById('registrationForm'));
+
+        // Convert FormData to URL-encoded string
+        /*var data = '';
+        formData.forEach(function(value, key) {
+            data += encodeURIComponent(key) + '=' + encodeURIComponent(value) + '&';
+        });
+        data.slice(0, -1);*/
+
+        // Send the request with the form data in the body
+        xmlHttp.send(formData);
+        
+        // Handle response from the server
+        xmlHttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status === 200) {
+                //document.getElementById('responseDiv').innerHTML = this.responseText;
+                alert(this.responseText);
+            } /*else {
+                alert('An error occurred: ' + this.status + ' ' + this.readyState);
+            }*/
+        };
+    }    
+</script>
 </body>
 </html>
-<style>

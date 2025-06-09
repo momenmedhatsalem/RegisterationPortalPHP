ee8d1f041d31dad83bda5cb223d0c1ece4600b9a
Habiba775 <125930000+Habiba775@users.noreply.github.com>
2025-06-09 20:32:52 +0300

    Update ExampleTest.php update some tests to eliminate warnings

--- LaravelRegisterationPortal/tests/Unit/ExampleTest.php
+++ LaravelRegisterationPortal/tests/Unit/ExampleTest.php
@@ -3,31 +3,32 @@
 namespace Tests\Unit;
 
 use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\Attributes\Test;
 
 class ExampleTest extends TestCase
 {
-    /** @test */
+    #[Test]
     public function password_must_be_at_least_8_characters()
     {
         $password = 'Pa@1';
         $this->assertLessThan(8, strlen($password), "Password should be at least 8 characters");
     }
 
-    /** @test */
+    #[Test]
     public function password_must_contain_a_number()
     {
         $password = 'Password!';
         $this->assertEquals(0, preg_match('/\d/', $password), "Password should contain at least one number");
     }
 
-    /** @test */
+    #[Test]
     public function password_must_contain_a_special_character()
     {
         $password = 'Password1';
         $this->assertEquals(0, preg_match('/[^a-zA-Z0-9]/', $password), "Password should contain at least one special character");
     }
 
-    /** @test */
+    #[Test]
     public function confirm_password_must_match_password()
     {
         $password = 'Pass@123';
@@ -35,9 +36,10 @@ class ExampleTest extends TestCase
         $this->assertEquals($password, $confirmPassword, "Confirm password should match password");
     }
 
-    /** @test */
+    #[Test]
     public function email_must_be_valid()
     {
         $email = 'user@example.com';
         $this->assertTrue(filter_var($email, FILTER_VALIDATE_EMAIL) !== false, "Email format is invalid");
     }
+}

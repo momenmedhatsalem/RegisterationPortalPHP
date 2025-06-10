3df719fc32159ae49d21a08117bcf653b3fd2877
maihassan1 <114602980+maihassan1@users.noreply.github.com>
2025-06-10 06:22:19 +0300

    Update web.php

--- LaravelRegisterationPortal/routes/web.php
+++ LaravelRegisterationPortal/routes/web.php
@@ -12,6 +12,7 @@ Route::post('/submit', [FormUserController::class, 'store']);
 Route::post('/check-whatsapp', [FormUserController::class, 'ajaxCheckWhatsApp'])->name('check.whatsapp');
 Route::get('/check-whatsapp', [FormUserController::class, 'ajaxCheckWhatsApp'])->name('check.whatsapp');
 
+Route::post('/check-unique', [App\Http\Controllers\UniquenessController::class, 'checkUnique']);
 
 // routes/web.php
 Route::get('/setlocale/{locale}', function ($locale) {
